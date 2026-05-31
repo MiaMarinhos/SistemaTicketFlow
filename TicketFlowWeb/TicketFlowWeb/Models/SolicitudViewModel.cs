89c3a90e07a9e46f3979f121bf764e8c64036de6
SergioVentura26 <a20232142@pucp.edu.pe>
2026-05-31 18:43:01 -0500

    SoliAnfitrion (razor y model)

---
+++ TicketFlowWeb/TicketFlowWeb/Models/SolicitudViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TicketFlowWeb.Models
+{
+    public class SolicitudViewModel
+    {
+        public string RazonSocial { get; set; } = string.Empty;
+        public string Ruc { get; set; } = string.Empty;
+        public string Telefono { get; set; } = string.Empty;
+        public string Correo { get; set; } = string.Empty;
+        public string Motivo { get; set; } = string.Empty;
+    }
+}
