ea97953959be40579fdd9a5179a8d27d7f9cded6
SergioVentura26 <a20232142@pucp.edu.pe>
2026-05-31 19:13:58 -0500

    CrearEvento (.razor y viewModel)

---
+++ TicketFlowWeb/TicketFlowWeb/Models/EventoViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TicketFlowWeb.Models
+{
+    public class EventoViewModel
+    {
+        public string Titulo { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public int NumeroEntradas { get; set; }
+        public decimal Precio { get; set; }
+        public string ImagenRuta { get; set; } = string.Empty;
+        public DateTime? Fecha { get; set; } = DateTime.Today;
+        public string HoraInicio { get; set; } = string.Empty;
+        public string HoraFin { get; set; } = string.Empty;
+        public string Ubicacion { get; set; } = string.Empty;
+        public string SitioEvento { get; set; } = string.Empty;
+        public string Distrito { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+    }
+}
