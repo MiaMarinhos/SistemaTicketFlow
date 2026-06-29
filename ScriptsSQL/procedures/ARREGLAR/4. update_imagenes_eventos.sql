7384fc7531a69b220615be2b30557a5c914b3705
josephandrader <joseph.andrader@pucp.edu.pe>
2026-06-28 23:31:29 -0500

    Updates de los eventos para ver las imagenes

---
+++ ScriptsSQL/procedures/ARREGLAR/4. update_imagenes_eventos.sql
@@ -0,0 +1,146 @@
+use ticket_flow;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/18Zfxsn7gQd4gqr874RTKWU_YkIB-DmXh'
+WHERE idEvento = 1;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1xU059ux7vUpvSPkv8pnLtVdwLNfIfTH3'
+WHERE idEvento = 2;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1xU059ux7vUpvSPkv8pnLtVdwLNfIfTH3'
+WHERE idEvento = 3;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/14guZ3fmv4T1dAwBcZyQbfw_su-cBlGF4'
+WHERE idEvento = 4;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1Ut8X5poUJVcOYxmxk4cq-OLrQ95S5wOM'
+WHERE idEvento = 5;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1YujrF6RRRvXAxZ8v5KfJ1qtb8V0ihOeq'
+WHERE idEvento = 6;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1mh4j5chpxULVhDRd_UCWrzC9Q3Ugz2Ic'
+WHERE idEvento = 7;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1bPcmMCiP-n9bmfCyFtxiSx4e7Kc_aGpI'
+WHERE idEvento = 8;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1D4hFZ6Bc4OoCCN6JwAUXroZefqwASoRj'
+WHERE idEvento = 9;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1fh1Z_2Fn3XJ8hIMEZU8ujzkSvfgqcWlg'
+WHERE idEvento = 10;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1ixMDNlH0LnkNcxFFmKtjEtMuLP_0eU2V'
+WHERE idEvento = 11;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1EMRbsMr8pfbckdBa1eQ97eRfhADftpSo'
+WHERE idEvento = 12;
+
+UPDATE evento
+SET img = 'https://lh3.googleusercontent.com/d/1mcWyQLHtEcRmBtxm0PuxYWTXQr4Tv_u6'
+WHERE idEvento = 13;
+
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1vskKrXYEvrmzATI8Sxg8fZsCrmsSBgjs'
+where idEvento = 14;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1nOvt4B3u-X6EJbQ2Thp3cmir3qUguC8q'
+where idEvento = 15;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1pvRVHN3LC4iHGSPqsSRienbryx8TyThE'
+where idEvento = 16;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1jKs0GESSldQ_nRS02BPcsIUJAI4vqYoU'
+where idEvento = 17;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1X4Hr9x5vQFq7unCjQo4ICDurWsQvxwhL'
+where idEvento = 18;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1xTfwhOsbyeWh9dUedf7W100jYo2-MxPo'
+where idEvento = 19;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1ffZngw_8tnQq35Q1dY7MmsJNcMyU-FPh'
+where idEvento = 20;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1SpEi12VVeTtX2vv0Mz-SpyBqIsQjsoMJ'
+where idEvento = 21;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1jfTq2niSTFuW6Y02JwGZ_KtQCg8nvJ68'
+where idEvento = 22;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1nEaj6noAubrcqcXAVZBspRg6HKygB5QJ'
+where idEvento = 23;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1VWVIQnqssvXNc4Y3JEmoTrh2oqDOQLlU'
+where idEvento = 24;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1WIzVt-qOnvH51GPUYBIswCITDgD94b-5'
+where idEvento = 25;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1I6goLhvh_lOImaykuRAREez9ud2lb6kT'
+where idEvento = 26;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/19TT_fcLh2BMew0zaGATaVMqex7IZTy2-'
+where idEvento = 27;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/17NrUGYpb8JAdnfnSKLE2DbQLMxQJ1ASA'
+where idEvento = 28;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1WJXSn8BwTCcxYlLA-88uZ6bwA2esea_J'
+where idEvento = 29;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1DhQxfGJ16CTs4ALCMyE9ChCMPagZf44_'
+where idEvento = 30;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1MSYuuZE_Snbe9Qd7Ejtzf-oT_zHheAUX'
+where idEvento = 31;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1V3eyV3e2SSPeW_6GtcIYCO69ff6kyZQ-'
+where idEvento = 32;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1kQgxVl-lDDp8tQc5MVvPHpSsLfFxJfQy'
+where idEvento = 33;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1r4JKAeWi9VthGasxnL4AFVxSycfcGKdJ'
+where idEvento = 34;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1hpY1KK6JNrz_wBrOJr8dVc4MRmkWZcK4'
+where idEvento = 35;
+
+UPDATE evento
+set img = 'https://lh3.googleusercontent.com/d/1Q2ub1Y4IwX2kXakHKCR57Sby3eZURabD'
+where idEvento = 36;
